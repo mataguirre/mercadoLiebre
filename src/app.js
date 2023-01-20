a10c789bffd9e6a43706de2eacae99ef4e009b2f
mataguirre <mataguirre.dev@gmail.com>
2023-01-20 04:41:24 -0300

    parameters-routes-controllers

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 //Debemos requerir las rutas creadas en "routes"
-const productsRoutes = require('./routes/productsRoutes.js');
-const mainRoutes = require('./routes/mainRoutes.js');
-let publicFolder = path.resolve(__dirname, './public');
+const productsRoutes = require('./routes/productsRoutes');
+const mainRoutes = require('./routes/mainRoutes');
+let publicFolder = path.resolve(__dirname, '../public');
 
 app.use(express.static(publicFolder));
 
