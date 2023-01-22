dd7d9728919230ba7ff64548f025d8312129dbda
mataguirre <mataguirre.dev@gmail.com>
2023-01-22 17:21:29 -0300

    changes commited

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const mainRoutes = require('./routes/mainRoutes');
 const port = process.env.PORT || 3000;
 //Debemos requerir las rutas creadas en "routes"
 let publicFolder = path.resolve(__dirname, '../public');
