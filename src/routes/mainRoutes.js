00ae7dcfa6d75c3e9604283f5dd1ccc2e336c4cb
mataguirre <mataguirre.dev@gmail.com>
2023-01-22 17:27:30 -0300

    changes commited

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -6,5 +6,6 @@ router.use('/', mainController.index);
 router.use('/home', mainController.index);
 router.use('/login', mainController.login);
 router.use('/register', mainController.register);
+router.use('/products', mainController.products);
 
 module.exports = router;
