23b4afd2da5914f9e5a05bb4578979c40de6ed28
mataguirre <mataguirre.dev@gmail.com>
2023-01-24 00:42:03 -0600

    issues fixed

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -4,7 +4,6 @@ const mainController = require('../controllers/mainController');
 const router = Router();
 
 router.use('/', mainController.index);
-router.use('/home', mainController.index);
 router.use('/login', mainController.login);
 router.use('/register', mainController.register);
 router.use('/products', mainController.products);
